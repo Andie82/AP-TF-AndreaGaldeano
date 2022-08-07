9f4ff0adebedc039567768fa500c9862cf042737
Andie <95512233+Andie82@users.noreply.github.com>
2022-08-07 15:05:23 -0300

    Backend

--- TrabajoFinal/Backend/ang/src/main/java/com/portfolio/ang/Interface/IPersonaService.java
+++ TrabajoFinal/Backend/ang/src/main/java/com/portfolio/ang/Interface/IPersonaService.java
@@ -7,6 +7,5 @@ public interface IPersonaService {
     public List<Persona> getPersona();
     public void savePersona(Persona persona);
     public void deletePersona(Long id);
-    public Persona findPersona(Long id);
-    
+    public Persona findPersona(Long id);   
 }
